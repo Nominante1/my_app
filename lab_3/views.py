698bd981384aad173c003ecb6ea27dfaf0c8a25e
Ivan <ivandavydov968@gmail.com>
2024-10-01 16:20:07 +0500

    It's working...

--- lab_3/views.py
+++ lab_3/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
-
+from .forms import PasswordForm
+from django.shortcuts import render
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello world")
+def password_view(request):
+    if request.method == 'POST':
+        form = PasswordForm(request.POST)
+        if form.is_valid():
+            # Здесь можно обработать валидный пароль
+            return render(request, 'lab_3/success.html')
+    else:
+        form = PasswordForm()
+    return render(request, 'lab_3/password_form.html', {'form': form})
